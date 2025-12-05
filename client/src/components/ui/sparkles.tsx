3d08b26e36d885610730e620fca9899e82e7954f
Tyler Williams <tylerbusinessza24@gmail.com>
2025-12-05 06:06:43 +0200

    Add client/src/components/ui/sparkles.tsx

---
+++ client/src/components/ui/sparkles.tsx
@@ -0,0 +1,162 @@
+"use client";
+import { useEffect, useId, useState, useRef } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import type { Container } from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
+import { cn } from "@/lib/utils";
+import { motion, useAnimation } from "framer-motion";
+
+type ParticlesProps = {
+  id?: string;
+  className?: string;
+  background?: string;
+  particleSize?: number;
+  minSize?: number;
+  maxSize?: number;
+  speed?: number;
+  particleColor?: string;
+  particleDensity?: number;
+};
+
+export const SparklesCore = (props: ParticlesProps) => {
+  const {
+    id,
+    className,
+    background,
+    minSize,
+    maxSize,
+    speed,
+    particleColor,
+    particleDensity,
+  } = props;
+  const [init, setInit] = useState(false);
+  const isMountedRef = useRef(false);
+
+  const controls = useAnimation();
+
+  useEffect(() => {
+    isMountedRef.current = true;
+    
+    initParticlesEngine(async (engine: any) => {
+      await loadSlim(engine);
+    }).then(() => {
+      if (isMountedRef.current) {
+        setInit(true);
+      }
+    });
+
+    return () => {
+      isMountedRef.current = false;
+    };
+  }, []);
+
+  const particlesLoaded = async (container?: Container) => {
+    if (container && isMountedRef.current) {
+      controls.start({
+        opacity: 1,
+        transition: {
+          duration: 1,
+        },
+      });
+    }
+  };
+
+  const generatedId = useId();
+
+  return (
+    <motion.div animate={controls} className={cn("opacity-0", className)}>
+      {init && (
+        <Particles
+          id={id || generatedId}
+          className={cn("h-full w-full")}
+          particlesLoaded={particlesLoaded}
+          options={{
+            background: {
+              color: {
+                value: background || "#0d47a1",
+              },
+            },
+            fullScreen: {
+              enable: false,
+              zIndex: 1,
+            },
+            fpsLimit: 120,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: false,
+                  mode: "repulse",
+                },
+                resize: true as any,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              bounce: {
+                horizontal: {
+                  value: 1,
+                },
+                vertical: {
+                  value: 1,
+                },
+              },
+              collisions: {
+                enable: false,
+                mode: "bounce",
+              },
+              color: {
+                value: particleColor || "#ffffff",
+              },
+              move: {
+                enable: true,
+                speed: speed || 2,
+                direction: "none",
+                random: false,
+                straight: false,
+                outModes: {
+                  default: "out",
+                },
+              },
+              number: {
+                value: particleDensity || 120,
+              },
+              opacity: {
+                value: {
+                  min: 0.1,
+                  max: 1,
+                },
+                animation: {
+                  enable: true,
+                  speed: 1,
+                  sync: false,
+                },
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: {
+                  min: minSize || 1,
+                  max: maxSize || 3,
+                },
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+      )}
+    </motion.div>
+  );
+};
